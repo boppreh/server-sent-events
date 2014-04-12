6c6002a7f31d6376220159f848383b40130ff517
BoppreH <boppreh@gmail.com>
2014-04-12 01:03:38 -0300

    Add publisher docs

--- sse.py
+++ sse.py
@@ -1,10 +1,23 @@
 from queue import Queue
 
 class Publisher(object):
+    """
+    Contains a list of subscribers that can can receive updates.
+    """
     def __init__(self):
+        """
+        Creates a new publisher with an empty list of subscribers.
+        """
         self.subscribers = []
 
     def publish(self, data):
+        """
+        Publishes data to all subscribers.
+
+        If data is callable, the return of `data(properties)` will be published
+        instead, for the `properties` object of each subscriber. This allows
+        for subscriber differentiation.
+        """
         if callable(data):
             for queue, properties in self.subscribers:
                 queue.put(data(properties))
@@ -13,6 +26,13 @@ class Publisher(object):
                 queue.put(data)
 
     def subscribe(self, properties=None):
+        """
+        Subscribes to the channel, returning an infinite generator of
+        Server-Sent-Events.
+
+        If `properties` is passed, these will be used for differentiation if a
+        callable object is published (see `Publisher.publish`).
+        """
         queue = Queue()
         properties = properties or {}
         self.subscribers.append((queue, properties))
