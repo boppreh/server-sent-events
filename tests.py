16fb0e4ceafc425379572afcff621925e2245aa2
BoppreH <boppreh@gmail.com>
2014-04-14 23:33:10 -0300

    Add test for multiple users

--- tests.py
+++ tests.py
@@ -12,5 +12,16 @@ class TestPublisher(unittest.TestCase):
         p.close()
         self.assertEqual(self.read(s), 'data: test')
 
+    def test_single_multiple(self):
+        p = Publisher()
+        s1 = p.subscribe()
+        s2 = p.subscribe()
+        s3 = p.subscribe()
+        p.publish('test')
+        p.close()
+        self.assertEqual(self.read(s1), 'data: test')
+        self.assertEqual(self.read(s2), 'data: test')
+        self.assertEqual(self.read(s3), 'data: test')
+
 if __name__ == '__main__':
     unittest.main()
