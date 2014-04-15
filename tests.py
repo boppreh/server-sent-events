4034f674a67c06fbbdada85a353a7471563aa9ae
BoppreH <boppreh@gmail.com>
2014-04-14 22:31:05 -0300

    Add initial test

--- tests.py
+++ tests.py
@@ -1,9 +1,16 @@
 import unittest
+from sse import Publisher
 
 class TestPublisher(unittest.TestCase):
+    def read(self, generator):
+        return ''.join(generator).strip()
+
     def test_single(self):
         p = Publisher()
-        p.subscribe
+        s = p.subscribe()
+        p.publish('test')
+        p.close()
+        self.assertEqual(self.read(s), 'data: test')
 
 if __name__ == '__main__':
     unittest.main()
