bf8daf57a1e2b10dfc2d4b3814db9035176bcd9a
BoppreH <boppreh@gmail.com>
2014-04-14 23:34:30 -0300

    Add channel test

--- tests.py
+++ tests.py
@@ -23,5 +23,19 @@ class TestPublisher(unittest.TestCase):
         self.assertEqual(self.read(s2), 'data: test')
         self.assertEqual(self.read(s3), 'data: test')
 
+    def test_channel(self):
+        p = Publisher()
+        s1 = p.subscribe('channel 1')
+        s2 = p.subscribe('channel 2')
+        s3 = p.subscribe(['channel 1', 'channel 2'])
+
+        p.publish('test1', 'channel 1')
+        p.publish('test2', 'channel 2')
+        p.close()
+
+        self.assertEqual(self.read(s1), 'data: test1')
+        self.assertEqual(self.read(s2), 'data: test2')
+        self.assertEqual(self.read(s3), 'data: test1\n\ndata: test2')
+
 if __name__ == '__main__':
     unittest.main()
